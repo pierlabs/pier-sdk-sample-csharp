0870a7facb61c3dcea9b1271db2feda4517b0d30
valber.paulino <valber.paulino@NJP0037>
2017-12-27 10:07:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Conductor.Pier.Api;
+using Conductor.Pier.Client;
+using Conductor.Pier.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ApiClient apiClient = new ApiClient(HOST_NAME);
+
+            String[] accerts = { "application/json" };
+
+            apiClient.SelectHeaderAccept(accerts);
+
+            ContaApi contaApi = new ContaApi(HOST_NAME);
+            contaApi.Configuration.AddDefaultHeader("access_token", ACCESS_TOKEN_CREFISA);
+            contaApi.Configuration.AddDefaultHeader("client_id", CLIENT_ID_CREFISA);
+            contaApi.Configuration.AddDefaultHeader("Host", HOST);
+            
+            var datePatter = "yyyy-MM-dd";
+            var timerPatter = "T00:00:00";
+
+            var dataAjuste = DateTime.Today.ToString(datePatter) + timerPatter;
+
+            ContaDetalheResponse contaDetalheResponse = contaApi.ConsultarUsingGET11(7);                                   
+
+            Console.WriteLine(contaDetalheResponse);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
